c7f06a55ca802203a4d9a41ce482dad950055c76
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2022-11-10 22:52:59 -0600

    added hacker rank solution

--- PrintLinkedList.js
+++ PrintLinkedList.js
@@ -46,3 +46,15 @@
  *
  */
 
+
+function printLinkedList(head) {
+
+	let currentNode = head;
+	while (currentNode != null) {
+		console.log(currentNode.data);
+		currentNode = currentNode.next;
+	}
+}
+
+//removed the first number
+
