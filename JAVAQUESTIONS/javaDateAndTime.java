0008c3f30c5a7b1740e1aed557ee9c56e143056d
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-01-21 02:54:01 -0600

    completed exercise

---
+++ JAVAQUESTIONS/javaDateAndTime.java
@@ -0,0 +1,24 @@
+package JAVAQUESTIONS;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class javaDateAndTime {
+
+    public static String findDay(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, month-1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        cal.set(Calendar.YEAR, year);
+        return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US).toUpperCase();
+    }
+}
