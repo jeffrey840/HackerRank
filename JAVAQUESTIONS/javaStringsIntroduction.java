139f9f124c5a079c37d38af399d349206acffdfb
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-03-09 01:41:08 -0600

    commiting hackerrank solution

--- JAVAQUESTIONS/javaStringsIntroduction.java
+++ JAVAQUESTIONS/javaStringsIntroduction.java
@@ -1,6 +1,8 @@
 package JAVAQUESTIONS;
 
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class javaStringsIntroduction {
 
@@ -9,7 +11,21 @@ public class javaStringsIntroduction {
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
+        /*
+first output will have the length of a nd b
+second will compare if the two strings
+third will print it out with a space
 
+
+         * */
+
+        /* Enter your code here. Print output to STDOUT. */
+        System.out.println((B + A).length());
+        System.out.println(A.compareTo(B)> 0 ? "Yes" : "No");
+        System.out.println(
+            A.substring(0,1).toUpperCase() + A.substring(1).toLowerCase()
+                +" "+
+            B.substring(0,1).toUpperCase() + B.substring(1).toLowerCase()
+        );
     }
 }
