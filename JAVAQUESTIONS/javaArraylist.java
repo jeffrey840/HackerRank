500e4cbef48b91dea64a8068978084f999d5dc98
jeffrey840 <Jeffrey.f.cabrera@gmail.com>
2023-05-19 23:40:12 -0500

    solved hackerrank question

---
+++ JAVAQUESTIONS/javaArraylist.java
@@ -0,0 +1,35 @@
+package JAVAQUESTIONS;
+
+import java.util.*;
+
+public class javaArraylist {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        ArrayList<ArrayList<Integer>> lines = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int d = scanner.nextInt();
+            ArrayList<Integer> line = new ArrayList<>();
+            for (int j = 0; j < d; j++) {
+                line.add(scanner.nextInt());
+            }
+            lines.add(line);
+        }
+
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            if (x <= lines.size() && y <= lines.get(x - 1).size()) {
+                System.out.println(lines.get(x - 1).get(y - 1));
+            } else {
+                System.out.println("ERROR!");
+            }
+        }
+
+        scanner.close();
+    }
+}
